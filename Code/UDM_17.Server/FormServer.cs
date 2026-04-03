f56b4752cde9ce0d3c3fedce51b05151c0c54074
phanduc-del <09zenka@gmail.com>
2026-04-03 12:36:05 +0700

    fix server bug

--- Code/UDM_17.Server/FormServer.cs
+++ Code/UDM_17.Server/FormServer.cs
@@ -1,73 +1,37 @@
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using UDM_17.Core;
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
 
-namespace UDM_17.Server
+namespace co_caro
 {
+ 
     public partial class FormServer : Form
     {
-        TcpListener listener;
-        List<TcpClient> clients = new List<TcpClient>();
+        ServerManager server = new ServerManager();
 
         public FormServer()
         {
             InitializeComponent();
-            this.Text = "UDM_17 - Caro Server Manager";
-            btnStartServer.Click += (s, e) => StartServer();
-            // Cho phép cập nhật UI từ luồng khác (Socket thread)
-            Control.CheckForIllegalCrossThreadCalls = false;
         }
 
-        void StartServer()
+        private void btnStart_Click(object sender, EventArgs e)
         {
-            try
+            new System.Threading.Thread(() =>
             {
-                listener = new TcpListener(IPAddress.Any, 1234);
-                listener.Start();
-                lstLog.Items.Add("[SYSTEM] Server đang lắng nghe tại cổng 1234...");
-                btnStartServer.Enabled = false;
-                btnStartServer.Text = "SERVER ONLINE";
+                server.Start(8888);
+            }).Start();
 
-                Task.Run(() => {
-                    while (true)
-                    {
-                        TcpClient client = listener.AcceptTcpClient();
-                        clients.Add(client);
-                        lstLog.Items.Add($"[+] Kết nối mới từ: {client.Client.RemoteEndPoint}");
-                        Task.Run(() => HandleClient(client));
-                    }
-                });
-            }
-            catch (Exception ex) { MessageBox.Show(ex.Message); }
+            MessageBox.Show("Server đang chạy!");
+        }
+        private void FormSever_Load(object sender, EventArgs e)
+        {
+
         }
 
-        void HandleClient(TcpClient client)
+        private void button1_Click(object sender, EventArgs e)
         {
-            NetworkStream ns = client.GetStream();
-            byte[] buf = new byte[2048];
-            while (true)
-            {
-                try
-                {
-                    int bytes = ns.Read(buf, 0, buf.Length);
-                    if (bytes == 0) break;
-                    string json = Encoding.UTF8.GetString(buf, 0, bytes);
-                    Broadcast(json);
-                }
-                catch { break; }
-            }
-            clients.Remove(client);
-            lstLog.Items.Add($"[-] Một người chơi đã thoát.");
-        }
-
-        void Broadcast(string msg)
-        {
-            byte[] data = Encoding.UTF8.GetBytes(msg);
-            foreach (var c in clients)
-            {
-                try { c.GetStream().Write(data, 0, data.Length); } catch { }
-            }
+            new Thread(() => server.Start(8888)).Start();
+            MessageBox.Show("Server đang chạy!");
         }
     }
 }
