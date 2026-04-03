828062ad1e39808a384a062c858d139e4563c779
phanduc-del <09zenka@gmail.com>
2026-04-03 12:36:51 +0700

    fix server bug

--- Code/UDM_17.Server/Program.cs
+++ Code/UDM_17.Server/Program.cs
@@ -1,16 +1,22 @@
-namespace UDM_17.Server;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-static class Program
+namespace co_caro
 {
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    static void Main()
+    internal static class Program
     {
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
-        ApplicationConfiguration.Initialize();
-        Application.Run(new FormServer());
-    }    
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormServer());
+        }
+    }
 }
