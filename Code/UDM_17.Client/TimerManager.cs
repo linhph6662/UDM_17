5e3e0136a4a8e347b15cec8edabc2821e6eb3ef9
Phan Hoai Linh <linhph6662@ut.edu.vn>
2026-04-09 16:49:01 +0700

    update TimerManager

--- Code/UDM_17.Client/TimerManager.cs
+++ Code/UDM_17.Client/TimerManager.cs
@@ -1,10 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Windows.Forms;
 
 namespace UDM_17.Client
 {
-    internal class TimerManager
+    public class TimerManager : IDisposable
     {
+        private Timer _timer;
+        public event EventHandler Tick;
+
+        public int Interval
+        {
+            get => _timer != null ? _timer.Interval : 0;
+            set { if (_timer != null) _timer.Interval = value; }
+        }
+
+        public bool IsRunning => _timer != null && _timer.Enabled;
+
+        public TimerManager(int interval = 1000)
+        {
+            _timer = new Timer();
+            _timer.Interval = interval;
+            _timer.Tick += (s, e) => Tick?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void Start()
+        {
+            _timer?.Start();
+        }
+
+        public void Stop()
+        {
+            _timer?.Stop();
+        }
+
+        public void Dispose()
+        {
+            if (_timer != null)
+            {
+                _timer.Stop();
+                _timer.Dispose();
+                _timer = null;
+            }
+        }
     }
 }
